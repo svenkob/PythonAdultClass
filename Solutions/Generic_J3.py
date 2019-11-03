e0874d7d8712f502fae6eba72f1e19e685597a21
svenkob <satish.venkob@gmail.com>
2019-11-03 09:19:46 -0500

    adding class01 files

---
+++ Solutions/Generic_J3.py
@@ -0,0 +1,16 @@
+file = open("inputJ3.txt", "r")
+word = file.readline()
+
+consonant =     "bcdfghjklmnpqrstvwxyz"
+closestVowel =  "aaeeeiiiiooooouuuuuuu"
+nextConsonant = "cdfghjklmnpqrstvwxyzz"
+
+newWord = ""
+for i in range(len(word)):
+    j = consonant.find (word[i])
+    newWord = newWord + word[i]  # the orginal letter is always copied over
+    if j > -1:                   # if it's not a vowel add the closest vowel and
+                                 # next consonant
+        newWord = newWord + closestVowel [j] + nextConsonant[j]
+
+print(newWord)
